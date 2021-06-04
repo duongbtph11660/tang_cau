9b2d81902d8c963006e9ae4cad4c91abb29d8fa7
duongbtph11660 <77599050+duongbtph11660@users.noreply.github.com>
2021-06-04 21:12:25 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,5 @@
-# Tặng Crush
-## _Một điều nho nhỏ tỏ tình với crush_
-
-Liên lạc: 
-[![Facebook](https://i.imgur.com/GRqy96ts.jpg)](https://www.facebook.com/nam.nodemy)
-[![Tiktok](https://i.imgur.com/Nbfl1E7t.jpg)](https://www.tiktok.com/@manindev)
-
-Mở file config.js sửa nội dung theo mẫu
-```
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hí cấu mình là Dương đây :)) !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -21,10 +12,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Cảm ơn cậu tớ biết mà . Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
-```
-
